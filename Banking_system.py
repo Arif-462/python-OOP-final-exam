3076d65bbc5c3b94a342eb3843af7a93b9b1c28d
Ariful Islam <116522948+Arif-462@users.noreply.github.com>
2023-08-06 02:33:37 +0300

    Update Banking_system.py

--- Banking_system.py
+++ Banking_system.py
@@ -7,21 +7,22 @@ class Customer(User):
     def __init__(self, name, account_number) -> None:
         super().__init__(name)
         self.account_number = account_number       
-        self.balance = 0
         self.Min_withdraw = 100
         self.max_windraw = 500000
         self.transaction_history = []
+        self.balance = 0
         self.loan_amount = 0
 
     def __repr__(self) -> str:
         return f'{self.name},{self.account_number}'
     
 
-    def deposite(self, amount):
+    def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            self.transaction_history.append(f'Deposite: {amount}')
-            print(f'Deposite: {amount}')
+            Banking_management_system.total_balance += amount
+            self.transaction_history.append(f'Deposit: {amount}')
+            print(f'Deposit: {amount}')
 
  
     def withdraw(self, amount):
@@ -33,19 +34,23 @@ class Customer(User):
 
             else:
                 self.balance -= amount
+                Banking_management_system.total_balance -= amount
                 self.transaction_history.append(f'withdraw: {amount}')
                 print(f'Congratulation! for withdraw {amount} from your {self.account_number}') 
         else:
             print('Your balance is not sufficient')
-            print('The bank is bankrupt')   
+            print('The bank is bankrupt')  
 
+  
     def get_balance(self):
        print(f'Your available balance is {self.balance}')
+       return self.balance
 
 
     def transfer_money(self, amount, other_account):
         if amount < self.balance:            
             self.balance -= amount
+            other_account += amount
             self.transaction_history.append(f'Transfer {amount} to account No: {other_account}')
             print(f'{amount} transfered from your account to {other_account}')
         else:
@@ -62,43 +67,55 @@ class Customer(User):
         if amount <= self.balance*2:
             self.balance += amount
             self.loan_amount += amount
-            print(f'Received {amount} loan to your account')
-            return amount
+            self.transaction_history.append(f'Received loan: {amount}')
+            Banking_management_system.total_loan += amount
+            Banking_management_system.total_balance -= amount
+            print(f'Received {amount} loan to your account')            
         else:
             print(f'You can not receive this {amount} as loan')
+            
+
+    def get_loan(self):
+        return self.loan_amount
+
 
 
 class Banking_management_system:
     def __init__(self):        
         self.accounts = {}
-        self.total_balance = 0
-        self.total_loan = 0
         self.loan_feature = True
+    total_balance = 0
+    total_loan = 0
+        
 
+        
     def create_account(self, name):
         account_number = random.randint(100, 10000)
-        while account_number in self.accounts:
-            account_number = random.randint(100, 10000)
-        self.accounts[account_number] = Customer(name, account_number)
+        for account_number in self.accounts:
+            account_number = random.randint(100000, 999999)
+        self.accounts[account_number] = self.accounts.get(account_number, name)
         print(f"Account Number: {account_number} created successfully.")
         return account_number
     
-    
-    def total_available_balance(self):          
-        for account in self.accounts.values():
-            self.total_balance += account.balance  
-        print(f"Total Available Balance is: {self.total_balance}")
 
-    def total_loan_amount(self):
-        for account in self.accounts.values():
-            self.total_loan += account.balance*2       
-        print(f"Total Loan is: {self.total_loan}")
+    def get_account(self, account_number):
+        # return self.accounts.get(account_number, None)
+        for account_number, name in self.accounts.items():
+            if account_number : name
+            return account_number
+        
+    
+    def total_available_balance(self):       
+        print(f"Total Available Balance is of the Bank : {self.total_balance}")
+
+    def total_loan_amount(self):             
+        print(f"Total Loan is of the Bank : {self.total_loan}")
 
     def loan_features(self):
         if self.loan_feature is True:
-            print("   off  ")
+            print("\t\t on")
         else:
-            print('   on   ')
+            print('\t\t of')
 
 
 
@@ -106,38 +123,42 @@ class Banking_management_system:
 
 admin = Banking_management_system()
 
-user = Customer('abc', admin.create_account('user'))
+print('\n---------- Created account Holder account----')
+act_no_1 = admin.create_account('user1')
+user = Customer('abc', act_no_1)
 
-print('=========Your diposited money==========================')
-user.deposite(12500)
+# print(admin.get_account('user'))
 
-print('=========Your withdrwal money==========================')
-user.withdraw(500000)
+print('\n--------------Your diposited money -------------------')
+user.deposit(12500)
 
-print('=========Your Transfer money to another account========')
-user.transfer_money(500, admin.create_account('other user'))
+print('\n\n--------------Your withdrwal money--------------------')
+user.withdraw(1000)
 
-print('=========Your Transaction History======================')
+print('\n--------------Your Transfer money to another account---')
+act_no_2 = admin.create_account('user2')
+user.transfer_money(500, act_no_2)
+
+print('\n--------------Your Transaction History-----------------')
 user.get_transaction_history()
 
-print('=========Your loan money===============================')
+print('\n--------------Your loan money--------------------------')
 user.receive_loan(2000)
 
-print('=========your balance==================================')
+print('\n--------------your Current balance---------------------')
 user.get_balance()
 
-print('========Admin can create a account=====================')
+print('\n--------------Admin can create a account---------------')
 admin.create_account('admin')
 
-print('========Admin check avialble balance===================')
+print('\n--------------Admin check available balance of the bank.')
 admin.total_available_balance()
 
-print('========Loan amount of admin========================== ')
+print('\n-------------Loan amount of admin----------------------')
 admin.total_loan_amount()
 
-print('========Loan features of Admin is now================= ')
+print('\n-------------Loan features of Admin is now ------------')
 admin.loan_features()
-
 
 
 
